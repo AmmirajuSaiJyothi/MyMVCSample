bb2b9091b8bf5a7b40670f97b44fc349aed3e21b
AmmirajuSaiJyothi <sjammiraju@gmail.com>
2017-12-27 14:51:09 +0530

    Add project files.

---
+++ MyMVCSample/Models/MVCClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMVCSample.Models
+{
+    public class MVCClass
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
